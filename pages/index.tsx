e9fb4afd18de40857a009aeeab73fd6471eca500
Suneka-vasan <suneka@glenise.in>
2022-11-25 13:14:32 +0530

    2nd commit-1
    Signed-off-by: Suneka-vasan <suneka@glenise.in>

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,8 +7,8 @@ import Footer from '../components/common/Footer'
 export default function Home() {
   return (
     <>
-      <Nav />
       <Meta />
+      <Nav />
       <Banner />
       <Footer />
     </>
